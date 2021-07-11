ccd4a08aff12c41925d46e088f5a4983ee1e7ab7
Brady Peterson <bpeterson1663@gmail.com>
2021-07-10 19:14:53 -0500

    feat(client): intial client files for ui

---
+++ client/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules|bower_components)/,
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                options: { presets: ['@babel/env'] },
+              },
+        ]
+    },
+    resolve: { extensions: [".js", ".jsx", ".ts", ".tsx", ".json"] },
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public/'),
+        compress: true,
+        port: 3000,
+
+    },
+    plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })],
+
+}
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const db = require('./db')
 const ShiftRouter = require('./routers/shift.router')
@@ -9,5 +10,9 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.use(express.json())
 app.use('/api', ShiftRouter)
+app.use(express.static(path.join(__dirname, 'client/build')))
 
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
