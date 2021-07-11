4e4be53d528424d5c3ab51e1a831ca1b2eca3ed7
Brady Peterson <bpeterson1663@gmail.com>
2021-07-10 19:59:20 -0500

    test(build): test build deployment change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App: React.FC = (): JSX.Element => {
     return (
         <div>
-            <h1>Hello World!!!</h1>
+            <h1>When I Work Demo</h1>
         </div>
     )
 }
