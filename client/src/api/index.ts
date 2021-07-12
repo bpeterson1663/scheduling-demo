39958239584fd2bb19dda23ab50165fa58360a93
Brady Peterson <bpeterson1663@gmail.com>
2021-07-11 21:12:08 -0500

    feat(api): set production api url

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from 'axios'
 import { SignInT } from '../types'
 
-const baseURL = process.env.LOCAL_URL ? `http://localhost:4000/api` : 'https://when-i-work-challenge.herokuapp.com/api'
+const baseURL = 'https://when-i-work-challenge.herokuapp.com/api'
 
 const api = axios.create({
   withCredentials: true,
