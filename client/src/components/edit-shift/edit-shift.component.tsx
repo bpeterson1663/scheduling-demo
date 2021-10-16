72834297a6fb664fd14bbc871bdc7ac662f7a40a
Brady Peterson <bpeterson1663@gmail.com>
2021-10-15 22:52:14 -0500

    feat(edit): hook up edit shift form with api

--- client/src/components/edit-shift/edit-shift.component.tsx
+++ client/src/components/edit-shift/edit-shift.component.tsx
@@ -1,94 +1,101 @@
 import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react'
-import { UserT, NewShiftT } from '../../types' 
+import { UserT, ShiftT } from '../../types'
 import { FormInputContainer, FormInputLabel, GroupContainer, FormSelectContainer } from '../form/form-input.styles'
 import { CustomButtonContainer } from '../form/button.styles'
-import { getHourMinute, getDateString } from '../../helpers'
+import { getHourMinute, getDateString, createEpoch, createEndTime } from '../../helpers'
+
 interface EditShiftProps {
-    employees: UserT[]
-    shift: NewShiftT
+  employees: UserT[]
+  shift: ShiftT
+  onSubmit: (id: string, shift: ShiftT) => void
 }
 
-const EditShiftForm: React.FC<EditShiftProps> = ({ employees, shift }) => {
-    const [name, setName] = useState('')
-    const [userId, setUserId] = useState('')
-    const [date, setDate] = useState('')
-    const [endTime, setEndTime] = useState('')
-    const [startTime, setStartTime] = useState('')
-    
-    useEffect(() => {
-        setName(shift.name)
-        setUserId(shift.userId)
-        setDate(getDateString(shift.startTime))
-        setEndTime(getHourMinute(shift.endTime))
-        setStartTime(getHourMinute(shift.startTime))
-    }, [])
-    const handleSubmit = (event: FormEvent) => {
+const EditShiftForm: React.FC<EditShiftProps> = ({ employees, shift, onSubmit }) => {
+  const [name, setName] = useState('')
+  const [userId, setUserId] = useState('')
+  const [date, setDate] = useState('')
+  const [endTime, setEndTime] = useState('')
+  const [startTime, setStartTime] = useState('')
 
-    }
-    
-    return (
-        <form onSubmit={handleSubmit}>
-        <GroupContainer>
-          <FormInputLabel htmlFor="name">Shift Name</FormInputLabel>
-          <FormInputContainer
-            name="name"
-            type="text"
-            required
-            value={name}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
-          />
-        </GroupContainer>
-        <GroupContainer>
-          <FormInputLabel htmlFor="userId">Select Employee</FormInputLabel>
-          <FormSelectContainer
-            name="userId"
-            value={userId}
-            onChange={(e: ChangeEvent<HTMLSelectElement>) => setUserId(e.target.value)}
-          >
-            <option disabled value="" hidden>
-              {' '}
-              -- select an option --{' '}
+  useEffect(() => {
+    setName(shift.name)
+    setUserId(shift.userId)
+    setDate(getDateString(shift.startTime))
+    setEndTime(getHourMinute(shift.endTime))
+    setStartTime(getHourMinute(shift.startTime))
+  }, [])
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault()
+
+    const epochStartTime = createEpoch(date, startTime)
+    const epochEndTime = createEndTime(epochStartTime, startTime, endTime)
+
+    onSubmit(shift._id, { ...shift, name, userId, startTime: epochStartTime, endTime: epochEndTime })
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <GroupContainer>
+        <FormInputLabel htmlFor="name">Shift Name</FormInputLabel>
+        <FormInputContainer
+          name="name"
+          type="text"
+          required
+          value={name}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
+        />
+      </GroupContainer>
+      <GroupContainer>
+        <FormInputLabel htmlFor="userId">Select Employee</FormInputLabel>
+        <FormSelectContainer
+          name="userId"
+          value={userId}
+          onChange={(e: ChangeEvent<HTMLSelectElement>) => setUserId(e.target.value)}
+        >
+          <option disabled value="" hidden>
+            {' '}
+            -- select an option --{' '}
+          </option>
+          {employees.map((employee: UserT) => (
+            <option key={employee._id} value={employee._id}>
+              {employee.lastName}, {employee.firstName}
             </option>
-            {employees.map((employee: UserT) => (
-              <option key={employee._id} value={employee._id}>
-                {employee.lastName}, {employee.firstName}
-              </option>
-            ))}
-          </FormSelectContainer>
-        </GroupContainer>
-        <GroupContainer>
-          <FormInputLabel htmlFor="date">Date</FormInputLabel>
-          <FormInputContainer
-            name="date"
-            type="date"
-            required
-            value={date}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => console.log(e.target.value)}
-          />
-        </GroupContainer>
-        <GroupContainer>
-          <FormInputLabel htmlFor="startTime">Start Time</FormInputLabel>
-          <FormInputContainer
-            name="startTime"
-            type="time"
-            value={startTime}
-            required
-            onChange={(e: ChangeEvent<HTMLInputElement>) => console.log(e.target.value)}
-          />
-        </GroupContainer>
-        <GroupContainer>
-          <FormInputLabel htmlFor="endTime">End Time</FormInputLabel>
-          <FormInputContainer
-            name="endTime"
-            type="time"
-            value={endTime}
-            required
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setEndTime(e.target.value)}
-          />
-        </GroupContainer>
-        <CustomButtonContainer type="submit" value="Create Shift" />
-      </form>
-    )
+          ))}
+        </FormSelectContainer>
+      </GroupContainer>
+      <GroupContainer>
+        <FormInputLabel htmlFor="date">Date</FormInputLabel>
+        <FormInputContainer
+          name="date"
+          type="date"
+          required
+          value={date}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setDate(e.target.value)}
+        />
+      </GroupContainer>
+      <GroupContainer>
+        <FormInputLabel htmlFor="startTime">Start Time</FormInputLabel>
+        <FormInputContainer
+          name="startTime"
+          type="time"
+          value={startTime}
+          required
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setStartTime(e.target.value)}
+        />
+      </GroupContainer>
+      <GroupContainer>
+        <FormInputLabel htmlFor="endTime">End Time</FormInputLabel>
+        <FormInputContainer
+          name="endTime"
+          type="time"
+          value={endTime}
+          required
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setEndTime(e.target.value)}
+        />
+      </GroupContainer>
+      <CustomButtonContainer type="submit" value="Edit Shift" />
+    </form>
+  )
 }
 
 export default EditShiftForm
--- client/src/pages/EditShift.page.tsx
+++ client/src/pages/EditShift.page.tsx
@@ -4,7 +4,7 @@ import { Dispatch } from 'redux'
 import { createStructuredSelector } from 'reselect'
 import { useParams } from 'react-router-dom'
 import { PageContainer, ContentContainer, PageTitle } from './pages.styles'
-import { fetchShiftStartAsync } from '../redux/shift/shift.actions'
+import { fetchShiftStartAsync, fetchShiftUpdateStartAsync } from '../redux/shift/shift.actions'
 import { selectFetchStatus, selectMessage, selectShift } from '../redux/shift/shift.selector'
 import {
   InitialEmployeeState,
@@ -13,12 +13,12 @@ import {
   InitialShiftState,
   InitialUserState,
   RoleT,
-  NewShiftT,
-  UserT
+  ShiftT,
+  UserT,
 } from '../types'
 import { selectRole } from '../redux/user/user.selector'
 import Spinner from '../components/spinner/spinner.component'
-import { ErrorMessage } from '../components/message/message.styles'
+import { ErrorMessage, SuccessMessage } from '../components/message/message.styles'
 import { selectEmployees } from '../redux/employee/employee.selector'
 import EditShiftForm from '../components/edit-shift/edit-shift.component'
 interface ParamTypes {
@@ -27,26 +27,31 @@ interface ParamTypes {
 
 interface EditShiftProps {
   getShift: (id: string) => void
-  shift: NewShiftT | null
+  updateShift: (id: string, payload: ShiftT) => void
+  shift: ShiftT | null
   fetchStatus: FetchStatusT
   message: MessageT
   employees: UserT[]
 }
 
-const EditShift: React.FC<EditShiftProps> = ({ getShift, shift, employees, fetchStatus, message }) => {
+const EditShift: React.FC<EditShiftProps> = ({ getShift, shift, employees, fetchStatus, message, updateShift }) => {
   const { id } = useParams<ParamTypes>()
 
   useEffect(() => {
     getShift(id)
   }, [])
-
+  const handleUpdateShift = (id: string, shift: ShiftT) => {
+    updateShift(id, shift)
+  }
   return (
     <PageContainer>
       <PageTitle>Edit Shift</PageTitle>
       {fetchStatus === 'loading' && <Spinner />}
       {fetchStatus === 'error' && message && <ErrorMessage>{message}</ErrorMessage>}
+      {fetchStatus === 'success' && message && <SuccessMessage>{message}</SuccessMessage>}
+
       <ContentContainer>
-        {shift && <EditShiftForm shift={shift} employees={employees}/>}
+        {shift && <EditShiftForm shift={shift} employees={employees} onSubmit={handleUpdateShift} />}
       </ContentContainer>
     </PageContainer>
   )
@@ -63,7 +68,7 @@ interface DesiredSelection {
   fetchStatus: FetchStatusT
   message: MessageT
   role: RoleT | undefined
-  shift: NewShiftT | null
+  shift: ShiftT | null
 }
 
 const mapStateToProps = createStructuredSelector<State, DesiredSelection>({
@@ -71,10 +76,11 @@ const mapStateToProps = createStructuredSelector<State, DesiredSelection>({
   fetchStatus: selectFetchStatus,
   message: selectMessage,
   role: selectRole,
-  employees: selectEmployees
+  employees: selectEmployees,
 })
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   getShift: (id: string) => dispatch<any>(fetchShiftStartAsync(id)),
+  updateShift: (id: string, payload: ShiftT) => dispatch<any>(fetchShiftUpdateStartAsync(id, payload)),
 })
 export default connect(mapStateToProps, mapDispatchToProps)(EditShift)
