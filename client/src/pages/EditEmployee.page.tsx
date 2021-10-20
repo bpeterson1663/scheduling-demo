502d95193230d15a92a1985e2be80ea773698210
Brady Peterson <bpeterson1663@gmail.com>
2021-10-19 22:43:52 -0500

    feat(employee): hook up redux connector

--- client/src/pages/EditEmployee.page.tsx
+++ client/src/pages/EditEmployee.page.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
-import { useHistory } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
+import { useHistory, useParams } from 'react-router-dom'
 import { PageContainer, PageTitle, ContentContainer, BackButton } from './pages.styles'
-export const EditEmployee = () => {
-  const history = useHistory()
+import { UserT, InitialEmployeeState, InitialShiftState, InitialUserState, FetchStatusT, MessageT,  } from '../types'
+import { createStructuredSelector } from 'reselect'
+import { selectEmployee, selectFetchStatus, selectMessage } from '../redux/employee/employee.selector'
+import { fetchEmployeeStartAsync } from '../redux/employee/employee.actions'
+
+interface ParamTypes {
+    id: string
+}
+
+interface EditEmployeeProps {
+    getEmployee: (id: string) => void
+    employee: UserT | null
+}
+
+export const EditEmployee: React.FC<EditEmployeeProps> = ({ getEmployee, employee }) => {
+    const { id } = useParams<ParamTypes>()
+    const history = useHistory()
+
+  useEffect(() => {
+    getEmployee(id)
+  }, [])
+  console.log("employee: ", employee)
+
   return (
     <PageContainer>
       <BackButton onClick={() => history.goBack()}>Back</BackButton>
@@ -13,4 +36,26 @@ export const EditEmployee = () => {
   )
 }
 
-export default EditEmployee
+interface State {
+    employeeReducer: InitialEmployeeState
+    shiftReducer: InitialShiftState
+    userReducer: InitialUserState
+  }
+  
+  interface DesiredSelection {
+    fetchStatus: FetchStatusT
+    message: MessageT
+    employee: UserT | null
+  }
+
+const mapStateToProps = createStructuredSelector<State, DesiredSelection>({
+  employee: selectEmployee,
+  fetchStatus: selectFetchStatus,
+  message: selectMessage,
+})
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  getEmployee: (id: string) => dispatch<any>(fetchEmployeeStartAsync(id))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditEmployee)
